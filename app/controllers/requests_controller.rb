f4cebeb4a2009eb04ed277e643712ac0997b54e7
Adam Miribyan <adam.miribyan@gmail.com>
2011-08-19 04:58:27 +0500

    Requests list for admins

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,10 @@
-class RequestsController < ApplicationController  
+class RequestsController < ApplicationController
+  http_basic_authenticate_with :name => "admin", :password => "breeandwine", :except => :create
+  
+  def index
+    @requests = Request.all
+  end
+  
   def create
     @client_request = Request.new(params[:request])
     @client_request.save
--- test/functional/requests_controller_test.rb
+++ test/functional/requests_controller_test.rb
@@ -1,13 +1,35 @@
 require 'test_helper'
 
 class RequestsControllerTest < ActionController::TestCase
-  setup do
-    @client_request = FactoryGirl.create(:request)
-  end
-  
-  should "created request" do
-    assert_difference('Request.count') do
-      post :create, decedent: @client_request.attributes
+  context "on POST to /requests" do
+    setup do
+      @client_request = FactoryGirl.create(:request)
+    end
+    
+    should "create request" do
+      assert_difference('Request.count') do
+        post :create, decedent: @client_request.attributes
+      end
     end
   end
+  
+  context "on GET to /requests" do
+    setup do
+      @request.env['HTTP_AUTHORIZATION'] = encode_credentials('admin', 'breeandwine')
+      get :index
+    end
+    
+    should "assign requests" do
+      assert_not_nil assigns(:requests)
+    end
+    
+    should respond_with(:success)    
+    should render_template('index')
+  end
+  
+  private
+
+    def encode_credentials(username, password)
+      "Basic #{ActiveSupport::Base64.encode64("#{username}:#{password}")}"
+    end
 end
