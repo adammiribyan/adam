86239e1e82ab31e5c21a2bcafe4d63cd0cdc130e
Adam Miribyan <adam.miribyan@gmail.com>
2011-08-24 10:36:36 +0500

    Translations

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Adam::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
   root :to => 'welcome#index'
-  match '/:locale' => 'welcome#index'
+  match '/:locale' => 'welcome#index', locale: /ru|cz/
   
   # Beautiful promo pages
   match '/:id' => 'pages#show'
