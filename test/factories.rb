788b9359d3a68418587678d926f889aec5bdf6c9
Adam Miribyan <adam.miribyan@gmail.com>
2011-08-25 23:21:16 +0500

    Post scaffold and factory

--- test/factories.rb
+++ test/factories.rb
@@ -2,4 +2,10 @@ FactoryGirl.define do
   factory :request do
     body 'thedoctor@adammiribyan.com'
   end
+  
+  factory :post do
+    title 'Prince and Pirate'
+    lead 'They.'
+    body 'They were.'
+  end
 end
